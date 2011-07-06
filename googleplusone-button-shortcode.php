beb80f12fce9d00d3711d0a27b0f35930a0b5a83
webgig <webgig@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-06 19:10:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/googleplusone-button/tags/1.0.1@405642 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- googleplusone-button-shortcode.php
+++ googleplusone-button-shortcode.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Google Plusone(+1) Button
- * @version 1.0
+ * @version 1.0.1
  * @author Sagar Bhandari <webgig.sagar@gmail.com>
  */
 
@@ -10,7 +10,7 @@
 add_shortcode( 'twg_gpo_button', 'twg_gpo_button_shortcode' );
 
 
-// [gpo_button]
+// [twg_gpo_button]
 function twg_gpo_button_shortcode( $atts ) {
 		 
 	return twg_gpo_button_main(); 
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: webgig, pravash chamling rai
 Plugin Site: http://www.thewebgig.com
 Tags: Google +1, Google Plusone(+1) Button , Google plus one,Google plus one button, Google +1 button 
 Requires at least: 3.0+
-Tested up to: 3.1.4
-Stable tag: 1.0.0
+Tested up to: 3.2
+Stable tag: 1.0.1
 
 == Description ==
 <br />
